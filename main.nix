01a91ceeb7265daf5b427c138e25b27938f910e6
Soham S Gumaste <sgumas2@uic.edu>
2022-06-23 14:14:33 -0500

    Add hardware description, going in for the install

--- main.nix
+++ main.nix
@@ -2,7 +2,13 @@
 
 {
 
-  imports = [./system/configuration.nix];
+  imports = [./system/hardware.nix];
+
+  # Enable Flakes
+  nix = {
+    package = pkgs.nixFlakes;
+    extraOptions = "experimental-features = nix-command flakes";
+  };
 
   users.motd = ''
     Hello random user, Welcome to LUG+ACM at UIC!\n
@@ -13,7 +19,43 @@
     extraGroups = ["users" "wheel"];
     createHome = true;
 
-  }
+    # Good luck hackers ;)
+    hashedPassword = "$6$fPQXxMm0MLDIYVzm$v.yav/eptB0vvNGYSDdaDtuTAszZzB8UVGaXOA/fFfOWEol.wu7vYSZVmNipfiJPvScxDOeTERriH5A0r3hu1.";
+  };
 
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  networking.firewall.enable = false;
+
+  environment.systemPackages = with pkgs; [
+    vim
+    emacs
+    coreutils
+    psmisc
+    gcc
+    firefox
+    git
+  ];
+
+  environment.variables = {
+    EDITOR = "vim";
+  };
+
+  services.openssh.enable = true;
+
+  time.timeZone = "America/Chicago";
+
+  services.fstrim.enable = true;
+
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  system.stateVersion = "22.05";
 
 }
---
+++ system/hardware.nix
@@ -0,0 +1,35 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  imports =
+    [ (modulesPath + "/installer/scan/not-detected.nix")
+    ];
+
+  boot.initrd.availableKernelModules = [ "uhci_hcd" "ehci_pci" "ahci" "xhci_pci" "mptsas" "usb_storage" "usbhid" "sd_mod" "sr_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-label/main";
+      fsType = "ext4";
+    };
+
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-label/esp";
+      fsType = "vfat";
+    };
+
+  swapDevices = [ ];
+
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp6s0.useDHCP = lib.mkDefault true;
+
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  # high-resolution display
+  hardware.video.hidpi.enable = lib.mkDefault true;
+}
